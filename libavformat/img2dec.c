6ea7bd2eca2ab1200e2face76ba3e680f512ec78
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-04-01 16:02:33 +0200

    avformat/img2dec: return immediately in jpeg_probe() when EOI is found
    Fixes probing of JPEG files containing MPF metadata appended at the end
    of the file.
    
    The MPF metadata chunk can contains multiple JPEG images (thumbnails)
    which makes the jpeg_probe fails (return 0) because it finds a SOI
    marker after EOI.

--- libavformat/img2dec.c
+++ libavformat/img2dec.c
@@ -795,8 +795,7 @@ static int jpeg_probe(const AVProbeData *p)
         case EOI:
             if (state != SOS)
                 return 0;
-            state = EOI;
-            break;
+            return AVPROBE_SCORE_EXTENSION + 1;
         case APP0:
             if (c == APP0 && AV_RL32(&b[i + 4]) == MKTAG('J','F','I','F'))
                 got_header = 1;
@@ -831,8 +830,6 @@ static int jpeg_probe(const AVProbeData *p)
         }
     }
 
-    if (state == EOI)
-        return AVPROBE_SCORE_EXTENSION + 1;
     if (state == SOS)
         return AVPROBE_SCORE_EXTENSION / 2 + got_header;
     return AVPROBE_SCORE_EXTENSION / 8 + 1;
