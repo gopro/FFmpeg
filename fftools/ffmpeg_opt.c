3206face15d55f014b6afe7e6b80bf77d0721e18
James Almer <jamrial@gmail.com>
2022-08-21 15:53:10 -0300

    fftools/ffmpeg_opt: try to propagate the requested output channel layout
    Don't silently replace it with the default layout for the amount of channels
    from the requested layout.
    
    Should fix ticket #9869
    
    Signed-off-by: James Almer <jamrial@gmail.com>
    (cherry picked from commit f5a663f2a9d05eddbd50609b45d92d2e39b3f9ed)

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -746,6 +746,38 @@ int assert_file_overwrite(const char *filename)
     return 0;
 }
 
+static void dump_attachment(AVStream *st, const char *filename)
+{
+    int ret;
+    AVIOContext *out = NULL;
+    const AVDictionaryEntry *e;
+
+    if (!st->codecpar->extradata_size) {
+        av_log(NULL, AV_LOG_WARNING, "No extradata to dump in stream #%d:%d.\n",
+               nb_input_files - 1, st->index);
+        return;
+    }
+    if (!*filename && (e = av_dict_get(st->metadata, "filename", NULL, 0)))
+        filename = e->value;
+    if (!*filename) {
+        av_log(NULL, AV_LOG_FATAL, "No filename specified and no 'filename' tag"
+               "in stream #%d:%d.\n", nb_input_files - 1, st->index);
+        exit_program(1);
+    }
+
+    assert_file_overwrite(filename);
+
+    if ((ret = avio_open2(&out, filename, AVIO_FLAG_WRITE, &int_cb, NULL)) < 0) {
+        av_log(NULL, AV_LOG_FATAL, "Could not open file %s for writing.\n",
+               filename);
+        exit_program(1);
+    }
+
+    avio_write(out, st->codecpar->extradata, st->codecpar->extradata_size);
+    avio_flush(out);
+    avio_close(out);
+}
+
 /* read file contents into a string */
 char *file_read(const char *filename)
 {
