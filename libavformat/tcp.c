93845601582edc4aae6f59da65faf0a3bfbaacb1
Henry Song <hsong@gopro.com>
2024-02-01 15:08:03 -0800

    avformat/tcp: fix for iOS platform to obtain both IPv4 and IPv6 addresses in tcp [MAGMA-3500]

--- libavformat/tcp.c
+++ libavformat/tcp.c
@@ -145,6 +145,12 @@ static int tcp_open(URLContext *h, const char *uri, int flags)
     }
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+#if TARGET_OS_IPHONE
+    /* Get both IPv4 and IPv6 addresses. This is to work around an appear
+       to be iOS bug that getaddr() does not return IPv6 and IPv4 addresses
+       using default ai_flag */
+    hints.ai_flags = AI_V4MAPPED | AI_ALL;
+#endif
     snprintf(portstr, sizeof(portstr), "%d", port);
     if (s->listen)
         hints.ai_flags |= AI_PASSIVE;
